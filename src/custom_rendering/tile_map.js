3555441f44dfcad99c54c7afe7d7b9fcb0923fab
codyThompson <cody.thompson22@gmail.com>
2015-12-29 00:56:48 -0800

    added skeleton update and draw to tile_map

--- src/custom_rendering/tile_map.js
+++ src/custom_rendering/tile_map.js
@@ -1,10 +1,20 @@
 var flatsim.TileMap = function (gl) {
-  this.vert_buffer = new flatsim.VertexBuffer(gl, 8); // TODO don't hardcode vertex count
-  this.face_buffer = new flatsim.VertexBuffer(gl, 6 * 3); // TODO don't hardcode face vert count
+  this.gl = gl;
+  this.vert_buffer = new flatsim.VertexBuffer(gl, 4); // TODO don't hardcode vertex count
+  this.face_buffer = new flatsim.VertexBuffer(gl, 2 * 3); // TODO don't hardcode face vert count
 };
 flatsim.TileMap.prototype = {
+  gl: null,
   vert_buffer: null,
   face_buffer: null,
 
   // TODO update and draw
+  update: function () {
+    this.vert_buffer.update();
+    this.face_buffer.update();
+  },
+
+  draw: function () {
+
+  },
 };

1412854238ec0c7e4cca8282e532b35ae38b0c2c
codyThompson <cody.thompson22@gmail.com>
2015-12-29 01:07:03 -0800

    Added basic shaders
    Added basic shader source and the gl code to compile them. This code
    hasn't been tested yet. The code makes use of multiline quotes so babel
    is required for compiling from es2015 to es5.

---
+++ src/custom_rendering/shaders.es6
@@ -0,0 +1,64 @@
+flatsim.Shaders = {
+  basic_vert_src: `
+    attribute vec3 aVertPos;
+    attribute vec3 aVertCol;
+
+    uniform mat4 projMat;
+
+    varying lowp vec4 vVertCol;
+
+    void main() {
+      gl_Position = projMat * vec4(aVertPos, 1.0);
+      vVertCol = aVertCol;
+    }
+  `, // end vertex shader
+
+  basic_frag_src: `
+    varying lowp vec4 vVertCol;
+
+    void main() {
+      gl_FragColor = vVertCol;
+    }
+  `, // end fragment shader
+
+  basic_vert_shader: null,
+  basic_frag_shader: null,
+  basic_program: null,
+
+  init: function (gl) {
+    this.basic_vert_shader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(this.basic_vert_shader, this.basic_vert_src);
+    gl.compileShader(this.basic_vert_shader);
+    if (!gl.getShaderParameter(this.basic_vert_shader, gl.COMPILE_STATUS)) {
+      flatsim.error(
+`Can't compile vertex shader
+${gl.getShaderInfoLog(this.basic_vert_shader)}
+(end compile error message)`
+      );
+      return;
+    }
+
+    // TODO remove code duplication
+    this.basic_frag_shader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(this.basic_frag_shader, this.basic_frag_src);
+    gl.compileShader(this.basic_frag_shader);
+    if (!gl.getShaderParameter(this.basic_frag_shader, gl.COMPILE_STATUS)) {
+      flatsim.error(
+`Can't compile fragment shader
+${gl.getShaderInfoLog(this.basic_frag_shader)}
+(end compile error message)`
+      );
+      return;
+    }
+
+    this.basic_program = gl.createProgram();
+    gl.attachShader(this.basic_program, this.basic_vert_shader);
+    gl.attachShader(this.basic_program, this.basic_frag_shader);
+    gl.linkProgram(this.basic_program);
+
+    if (!gl.getProgramParameter(this.basic_program, gl.LINK_STATUS)) {
+      flatsim.error('Can\'t link basic shader program');
+      return;
+    }
+  },
+};
---
+++ src/custom_rendering/shaders.js
@@ -0,0 +1,40 @@
+'use strict';
+
+flatsim.Shaders = {
+  basic_vert_src: '\n    attribute vec3 aVertPos;\n    attribute vec3 aVertCol;\n\n    uniform mat4 projMat;\n\n    varying lowp vec4 vVertCol;\n\n    void main() {\n      gl_Position = projMat * vec4(aVertPos, 1.0);\n      vVertCol = aVertCol;\n    }\n  ', // end vertex shader
+
+  basic_frag_src: '\n    varying lowp vec4 vVertCol;\n\n    void main() {\n      gl_FragColor = vVertCol;\n    }\n  ', // end fragment shader
+
+  basic_vert_shader: null,
+  basic_frag_shader: null,
+  basic_program: null,
+
+  init: function init(gl) {
+    this.basic_vert_shader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(this.basic_vert_shader, this.basic_vert_src);
+    gl.compileShader(this.basic_vert_shader);
+    if (!gl.getShaderParameter(this.basic_vert_shader, gl.COMPILE_STATUS)) {
+      flatsim.error('Can\'t compile vertex shader\n' + gl.getShaderInfoLog(this.basic_vert_shader) + '\n(end compile error message)');
+      return;
+    }
+
+    // TODO remove code duplication
+    this.basic_frag_shader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(this.basic_frag_shader, this.basic_frag_src);
+    gl.compileShader(this.basic_frag_shader);
+    if (!gl.getShaderParameter(this.basic_frag_shader, gl.COMPILE_STATUS)) {
+      flatsim.error('Can\'t compile fragment shader\n' + gl.getShaderInfoLog(this.basic_frag_shader) + '\n(end compile error message)');
+      return;
+    }
+
+    this.basic_program = gl.createProgram();
+    gl.attachShader(this.basic_program, this.basic_vert_shader);
+    gl.attachShader(this.basic_program, this.basic_frag_shader);
+    gl.linkProgram(this.basic_program);
+
+    if (!gl.getProgramParameter(this.basic_program, gl.LINK_STATUS)) {
+      flatsim.error('Can\'t link basic shader program');
+      return;
+    }
+  }
+};

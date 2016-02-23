dc89cbde56f4073f45a9202adda0df0e64c8bc3c
Cody Thompson <cody.thompson22@gmail.com>
2016-02-22 20:43:35 -0800

    Added some basic initialization code to LayerBuffer

--- src/renderer2/layer_buffer.js
+++ src/renderer2/layer_buffer.js
@@ -5,5 +5,21 @@
  *
  * should maybe have an add and update face method
  */
-flatsim.LayerBuffer = function (gl, verts, faces, colors, norms) {};
-flatsim.LayerBuffer.prototype = {};
+flatsim.LayerBuffer = function (gl, verts, faces, colors, norms) {
+  this.gl = gl;
+  this.vert_buffer = new flatsim.ArrayBuffer(gl, verts);
+  this.face_buffer = new flatsim.ArrayBuffer(gl, faces, gl.ELEMENT_ARRAY_BUFFER);
+  this.norm_buffer = new flatsim.ArrayBuffer(gl, norms);
+  this.color_buffer = new flatsim.ArrayBuffer(gl, colors, 4);
+};
+flatsim.LayerBuffer.prototype = {
+  gl: null,
+  vert_buffer: null,
+  face_buffer: null,
+  norm_buffer: null,
+  color_buffer: null,
+
+  set_face: function (verts, faces, colors, norms, faceIx) {
+    // TODO
+  }
+};

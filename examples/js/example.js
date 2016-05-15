24bd1326b7cc84a0fd78d0dbbc7429a403dff99a
Cody Thompson <cody.thompson22@gmail.com>
2016-05-15 12:57:32 -0700

    Major Refactor of entire flatsim lib
    Renaming to gltile and attempting to use lib_scope for the scope of all
    future gltile objects
    
    Changing dir structure a bit
    
    starting from scratch

---
+++ examples/js/example.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+  console.log('gltile', gltile.version);
+  // TODO
+
+  var gt_canvas = document.getElementById('gltile-canvas');
+  var gt_gl = gt_canvas.getContext('webgl', {});
+});
--- todo.md
+++
@@ -1,7 +0,0 @@
-#TODO
-## TileMapMesh TileMapGeo
-1. Refactor the hell out of, and possibly combine and/or rename
-
-## Tile tile.js
-1. add field(s) for grid opacity and presence
-

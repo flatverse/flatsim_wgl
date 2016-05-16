66902092959092850ee79792f5222918c166fc49
codyThompson <cody.thompson22@gmail.com>
2016-05-15 17:38:58 -0700

    added a generalized way to throw an error.

--- src/gltile.js
+++ src/gltile.js
@@ -1,7 +1,21 @@
 (function (scope) {
 
 scope.gltile = {
-  version: '0.0.0'
+  name: 'gltile',
+  version: '0.0.0',
+
+  trace_message: function (message) {
+    var final_msg = '[' + this.name + ']';
+    for (var i = 1; i < arguments.length; i++) {
+      final_msg += '[' + arguments[i] + ']';
+    }
+    final_msg += ' ' + message;
+    return final_msg;
+  },
+
+  except: function (message) {
+    throw this.trace_message.apply(this, arguments);
+  }
 };
 
 }(window));

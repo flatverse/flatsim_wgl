323bc734aa1b00c3bac24b260ac50b00a2e04b03
codyThompson <cody.thompson22@gmail.com>
2016-05-15 17:56:42 -0700

    Added basic shader compiling.

--- examples/js/example.js
+++ examples/js/example.js
@@ -5,9 +5,5 @@ window.addEventListener('load', function () {
   var gt_canvas = document.getElementById('gltile-canvas');
   var gt_gl = gt_canvas.getContext('webgl', {});
 
-  var t = new gltile.Tile({
-    i: 0,
-    j: 0,
-    k: 0
-  });
+  gltile.shaders.init(gt_gl);
 });
--- src/shaders/shaders.js
+++ src/shaders/shaders.js
@@ -1,7 +1,34 @@
 (function (scope){
 
 scope.shaders = {
+  // this is filled by the individual shader js files
   sources: {},
+
+  init: function (gl_context) {
+    var simple_vert = this.compile_src(gl_context, this.sources.simple_color_vert, gl_context.VERTEX_SHADER);
+    var simple_frag = this.compile_src(gl_context, this.sources.simple_color_frag, gl_context.FRAGMENT_SHADER);
+
+    this.simple_color_shader = gl_context.createProgram();
+    gl_context.attachShader(this.simple_color_shader, simple_vert);
+    gl_context.attachShader(this.simple_color_shader, simple_frag);
+    gl_context.linkProgram(this.simple_color_shader);
+
+    if (!gl_context.getProgramParameter(this.simple_color_shader, gl_context.LINK_STATUS)) {
+      scope.except('Can\'t link basic shader program');
+      return;
+    }
+  },
+
+  compile_src: function(gl_context, src, shader_type) {
+    var shader = gl_context.createShader(shader_type);
+    gl_context.shaderSource(shader, src);
+    gl_context.compileShader(shader);
+    if (!gl_context.getShaderParameter(shader, gl_context.COMPILE_STATUS)) {
+      scope.except(gl_context.getShaderInfoLog(shader), 'shaders', 'compiling_src');
+    }
+
+    return shader;
+  },
 };
 
 })(window.gltile);

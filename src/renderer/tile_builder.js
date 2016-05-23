0aaa35c786e624bebbf45f5227565e486ebe037b
Cody Thompson <cody.thompson22@gmail.com>
2016-05-22 21:09:45 -0700

    Cleaned up TileBuilder
    - renamed vars from size_ to scale_
    - moved template ver arrays from prototype to constructor so that they
      don't function like static variables.

--- src/renderer/tile_builder.js
+++ src/renderer/tile_builder.js
@@ -2,50 +2,52 @@
 
 scope.TileBuilder = function (options) {
   _.extend(this, _.defaults(options || {}, {
-    size_we: 1,
-    size_ns: 1,
-    size_tb: 1,
+    scale_we: 1,
+    scale_ns: 1,
+    scale_tb: 1,
   }));
-};
-scope.TileBuilder.prototype = {
-  face_west: new Float32Array([
+
+  this.face_west = new Float32Array([
     -0.5,  0.5, -0.5, // lt wnb
     -0.5, -0.5, -0.5, // lb wsb
     -0.5, -0.5,  0.5, // rb wst
     -0.5,  0.5,  0.5, // rt wnt
-  ]),
-  face_east: new Float32Array([
+  ]);
+  this.face_east = new Float32Array([
      0.5,  0.5,  0.5, // lt ent
      0.5, -0.5,  0.5, // lb est
      0.5, -0.5, -0.5, // rb esb
      0.5,  0.5, -0.5, // rt enb
-  ]),
+  ]);
 
-  face_north: new Float32Array([
+  this.face_north = new Float32Array([
     -0.5,  0.5, -0.5, // lt wnb
     -0.5,  0.5,  0.5, // lb wnt
      0.5,  0.5,  0.5, // rb ent
      0.5,  0.5, -0.5, // rt enb
-  ]),
-  face_south: new Float32Array([
+  ]);
+  this.face_south = new Float32Array([
     -0.5, -0.5,  0.5, // lt wst
     -0.5, -0.5, -0.5, // lb wsb
      0.5, -0.5, -0.5, // rb esb
      0.5, -0.5,  0.5, // rt est
-  ]),
+  ]);
 
-  face_top: new Float32Array([
+  this.face_top = new Float32Array([
     -0.5,  0.5,  0.5, // lt wnt
     -0.5, -0.5,  0.5, // lb wst
      0.5, -0.5,  0.5, // rb est
      0.5,  0.5,  0.5, // rt ent
-  ]),
-  face_bottom: new Float32Array([
+  ]);
+  this.face_bottom = new Float32Array([
      0.5,  0.5, -0.5, // lt  enb
      0.5, -0.5, -0.5, // lb  esb
     -0.5, -0.5, -0.5, // rb  wsb
     -0.5,  0.5, -0.5, // rt  wnb
-  ]),
+  ]);
+};
+scope.TileBuilder.prototype = {
+  // TODO vert builders
 };
 
 })(gltile);

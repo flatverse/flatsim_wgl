dac53cfcccc9ed2a9cbde8d3c293f7cf6194f836
codyThompson <cody.thompson22@gmail.com>
2016-05-30 15:01:22 -0700

    added a util function for converting matrix arrays to a formatted string.

--- src/utils.js
+++ src/utils.js
@@ -16,6 +16,37 @@ scope.Utils = {
     }
     return final_array;
   },
+
+  mat_string: function (mat, columns, rows, optional_sep) {
+    optional_sep = optional_sep || ' ';
+
+    var formatted = [];
+    var max_digits = 0;
+    for (var i = 0; i < mat.length; i++) {
+      var val = '' + Math.round(mat[i] * 100) / 100;
+      formatted[i] = val;
+      if (val.length > max_digits) {
+        max_digits = val.length;
+      }
+    }
+
+    var str = '\n';
+    for (var row = 0; row < rows; row++) {
+      for (var column = 0; column < columns; column++) {
+        var val = formatted[(row * columns) + column];
+        var pad_width = max_digits - val.length;
+        for (var i = 0; i < pad_width; i++) {
+          val = ' ' + val;
+        }
+        str += val;
+        if (column < columns - 1) {
+          str += optional_sep;
+        }
+      }
+      str += '\n';
+    }
+    return str;
+  }
 };
 
 })(window.gltile);

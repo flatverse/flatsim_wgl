5f4796aea018bc2b719857847fc51712357646b6
codyThompson <cody.thompson22@gmail.com>
2016-06-17 20:33:59 -0700

    added grid lines to the default shader.

--- src/shaders/simple_color_frag.glsl
+++ src/shaders/simple_color_frag.glsl
@@ -1,5 +1,11 @@
 varying lowp vec4 fragVertColor;
+varying lowp vec4 fragVertIx;
 
 void main() {
-  gl_FragColor = fragVertColor;
+  // adapted from http://codeflow.org/entries/2012/aug/02/easy-wireframe-display-with-barycentric-coordinates/
+  if (any(lessThan(fragVertIx, vec4(0.02)))) {
+    gl_FragColor = vec4(0, 0, 1, 1);
+  } else {
+    gl_FragColor = fragVertColor;
+  }
 }
--- src/shaders/simple_color_vert.glsl
+++ src/shaders/simple_color_vert.glsl
@@ -1,13 +1,16 @@
 attribute vec3 vertPos;
 attribute vec4 vertColor;
+attribute vec4 vertIx;
 
 uniform mat4 projMat;
 uniform mat4 mvMat;
 
+varying lowp vec4 fragVertIx;
 varying lowp vec4 fragVertColor;
 
 void main() {
   vec4 mvPos = mvMat * vec4(vertPos, 1.0);
   gl_Position = projMat * mvPos;
   fragVertColor = vertColor;
+  fragVertIx = vertIx;
 }
